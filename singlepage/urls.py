bfe4c22469455461a0894e55db4391f13b2a54af
marisa-21903611 <marisapintosantoss@gmail.com>
2021-07-03 22:27:26 +0100

    submissao

--- singlepage/urls.py
+++ singlepage/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "singlepage"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("sections/<int:num>", views.section, name="section")
