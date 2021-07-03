48c4826da5f0fcff5500d139b73a009b257ac442
marisa-21903611 <marisapintosantoss@gmail.com>
2021-07-04 00:35:33 +0100

    submissao

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
